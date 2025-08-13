fe8fee31dd4f443c43ba4189b2386ee7454f4232
amrkhalid <muslimidentity12@gmail.com>
2025-08-13 19:43:40 +0300

    before moving to tut-005

--- src/routes/base.py
+++ src/routes/base.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI , APIRouter
+from helpers.config import get_settings
 import os
 base_router = APIRouter(prefix="/api/v1" , 
                         tags=["api_v1"])
@@ -6,8 +7,9 @@ base_router = APIRouter(prefix="/api/v1" ,
 
 @base_router.get("/")
 async def welcome():
-    app_name =os.getenv('APP_NAME')
-    app_version = os.getenv('APP_version')
+    app_settings = get_settings()
+    app_name = app_settings.APP_Name
+    app_version = app_settings.APP_Version
     return {
         "app_name " : app_name,
         "app_version " : app_version , 
